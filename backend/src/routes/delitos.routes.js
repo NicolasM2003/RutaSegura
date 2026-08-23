5a2aad31dab44d1ee91cc70693a0dc49988ba00c
NicolasM2003 <nicolaj354@gmail.com>
2026-08-23 04:31:06 -0400

    Revert "feat: implementar endpoint de consulta de delitos"

--- backend/src/routes/delitos.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { obtenerDelitos } = require("../controllers/delitos.controller");
-
-const router = express.Router();
-
-router.get("/", obtenerDelitos);
-
-module.exports = router;
